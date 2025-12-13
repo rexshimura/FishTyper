93f53aa4b292c428036e6301c8f3779908e046c4
Rexshimura <mortpauljpm03@gmail.com>
2025-12-13 23:07:18 +0800

    update 1.2

---
+++ config.js
@@ -0,0 +1,204 @@
+const fishConfig = [
+    {
+        type: "guppy",
+        imgSrc: "public/3.png",
+        speed: 0.8,
+        minDepth: 0,
+        maxDepth: 300,      // STOPS appearing after 300m
+        spawnWeight: 100,   // VERY COMMON
+        lenMin: 1,
+        lenMax: 1,
+        scale: 0.9,
+        scoreBonus: 0,
+        effect: null
+    },
+    {
+        type: "g_guppy",
+        imgSrc: "public/4.png",
+        speed: 1.5,
+        minDepth: 150,
+        maxDepth: null,     // null means it never stops appearing
+        spawnWeight: 5,     // VERY RARE (5 vs 100)
+        lenMin: 1,
+        lenMax: 1,
+        scale: 1.0,
+        scoreBonus: 50,
+        effect: "gold"
+    },    
+    {
+        type: "puffer",
+        imgSrc: "public/6.png",
+        speed: 0.6,
+        minDepth: 50,
+        maxDepth: 200,      // Stops at 400m
+        spawnWeight: 60,    // Common
+        lenMin: 2,
+        lenMax: 2,
+        scale: 1.2,
+        scoreBonus: 5,
+        effect: null
+    },
+    {
+        type: "clownfish",
+        imgSrc: "public/5.png",
+        speed: 0.7,
+        minDepth: 80,
+        maxDepth: 200,      // Stops at 400m
+        spawnWeight: 50,    // Common
+        lenMin: 2,
+        lenMax: 2,
+        scale: 1.3,
+        scoreBonus: 5,
+        effect: null
+    },
+    {
+        type: "piranha",
+        imgSrc: "public/7.png",
+        speed: 1.2,
+        minDepth: 100,
+        maxDepth: 400,      // Stops at 400m
+        spawnWeight: 80,    // Common
+        lenMin: 3,
+        lenMax: 3,
+        scale: 1.6,
+        scoreBonus: 8,
+        effect: null
+    },
+    {
+        type: "bass",
+        imgSrc: "public/8.png",
+        speed: 1.0,
+        minDepth: 80,
+        maxDepth: 400,      // Stops at 400m
+        spawnWeight: 100,    // Common
+        lenMin: 4,
+        lenMax: 4,
+        scale: 2.2,
+        scoreBonus: 10,
+        effect: null
+    },     
+    {
+        type: "salmon",
+        imgSrc: "public/9.png",
+        speed: 1.3,
+        minDepth: 90,
+        maxDepth: 400,      // Stops at 400m
+        spawnWeight: 100,    // Common
+        lenMin: 4,
+        lenMax: 5,
+        scale: 2.2,
+        scoreBonus: 12,
+        effect: null
+    },      
+    {
+        type: "barracuda",
+        imgSrc: "public/10.png",
+        speed: 1.5,
+        minDepth: 170,
+        maxDepth: 500,      
+        spawnWeight: 100,    // Common
+        lenMin: 5,
+        lenMax: 5,
+        scale: 2.8,
+        scoreBonus: 12,
+        effect: null
+    },                
+    {
+        type: "tuna",
+        imgSrc: "public/11.png",
+        speed: 2.5,
+        minDepth: 170,
+        maxDepth: null,
+        spawnWeight: 50,    // Uncommon
+        lenMin: 3,
+        lenMax: 5,
+        scale: 3.0,
+        scoreBonus: 15,
+        effect: null
+    },
+    {
+        type: "sunfish",
+        imgSrc: "public/12.png",
+        speed: 0.9,
+        minDepth: 100,
+        maxDepth: 500,
+        spawnWeight: 10,
+        lenMin: 6,
+        lenMax: 6,
+        scale: 5,
+        scoreBonus: 20,
+        effect: null
+    },
+    {
+        type: "marlin",
+        imgSrc: "public/13.png",
+        speed: 2.2,
+        minDepth: 380,
+        maxDepth: null,
+        spawnWeight: 50,
+        lenMin: 4,
+        lenMax: 4,
+        scale: 3.2,
+        scoreBonus: 28,
+        effect: null
+    },
+    {
+        type: "shark",
+        imgSrc: "public/14.png",
+        speed: 1.2,
+        minDepth: 600,
+        maxDepth: null,
+        spawnWeight: 25,    // Rare dangerous enemy
+        lenMin: 7,
+        lenMax: 8,
+        scale: 6,
+        scoreBonus: 50,
+        effect: null
+    },
+    // {
+    //     type: "ray",
+    //     imgSrc: "public/16.png",
+    //     speed: 1.3,
+    //     minDepth: 400,
+    //     maxDepth: 600,
+    //     spawnWeight: 50,    // Rare dangerous enemy
+    //     lenMin: 7,
+    //     lenMax: 7,
+    //     scale: 4,
+    //     scoreBonus: 18,
+    //     effect: null
+    // },
+    {
+        type: "whale",
+        imgSrc: "public/15.png",
+        speed: 0.5,
+        minDepth: 800,
+        maxDepth: null,
+        spawnWeight: 15,    // Very Rare Boss
+        lenMin: 10,
+        lenMax: 12,
+        scale: 8.0,
+        scoreBonus: 15,
+        effect: null
+    },
+    {
+        type: "jelly",
+        imgSrc: "public/18.png",
+        speed: 0.7,
+        minDepth: 500,
+        maxDepth: null,
+        spawnWeight: 35,    // Very Rare Boss
+        lenMin: 5,
+        lenMax: 5,
+        scale: 4,
+        scoreBonus: 100,
+        effect: null
+    }
+];
+
+const gameSettings = {
+    initialSpawnRate: 2000, 
+    minSpawnRate: 800,      
+    depthIncreaseRate: 5,   
+    pointsPerChar: 2
+};
---
+++ words.js
@@ -0,0 +1,14 @@
+const wordList = {
+    1: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+    2: ['at', 'in', 'on', 'up', 'do', 'go', 'hi', 'me', 'my', 'no', 'of', 'ox', 'to', 'us', 'we'],
+    3: ['cat', 'dog', 'sun', 'run', 'red', 'sea', 'box', 'bat', 'ant', 'bee', 'cow', 'fox', 'pig', 'rat'],
+    4: ['fish', 'blue', 'swim', 'deep', 'dark', 'sand', 'crab', 'clam', 'wave', 'ship', 'boat', 'sail'],
+    5: ['shark', 'whale', 'ocean', 'beach', 'coral', 'diver', 'water', 'storm', 'depth', 'light'],
+    6: ['pirate', 'anchor', 'island', 'vessel', 'sailor', 'oyster', 'shrimp', 'squids', 'jelly'],
+    7: ['dolphin', 'captain', 'lobster', 'seaside', 'current', 'tsunami', 'surface', 'fishing'],
+    8: ['stingray', 'plankton', 'starfish', 'barnacle', 'seahorse', 'predator', 'maritime'],
+    9: ['jellyfish', 'submarine', 'coastline', 'shipwreck', 'whirlpool', 'harpooner'],
+    10:['lighthouse', 'underwater', 'barracuda', 'expedition', 'navigation'],
+    11:['oceaniclife', 'treacherous', 'unsinkable', 'shipbuilder'],
+    12:['oceanography', 'invertebrate', 'mediterranean', 'biodiversity']
+};
